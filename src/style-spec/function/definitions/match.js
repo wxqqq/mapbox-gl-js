ca394970aee06d426ffb4fa6af8295958b839f54
Anand Thakker <github@anandthakker.net>
2017-08-08 08:46:15 -0400

    Address review comments re: Match

--- src/style-spec/function/definitions/match.js
+++ src/style-spec/function/definitions/match.js
@@ -1,13 +1,14 @@
 // @flow
 
 const assert = require('assert');
-const { parseExpression, match } = require('../expression');
+const { parseExpression, checkSubtype } = require('../expression');
 const { typeOf } = require('../values');
 
 import type { Expression, ParsingContext } from '../expression';
 import type { Type } from '../types';
 
-type Branches = Array<[Array<null | number | string | boolean>, Expression]>;
+// Map input label values to output expression index
+type Cases = {[number | string | boolean]: number};
 
 class Match implements Expression {
     key: string;
@@ -15,29 +16,31 @@ class Match implements Expression {
     inputType: Type;
 
     input: Expression;
-    branches: Branches;
+    cases: Cases;
+    outputs: Array<Expression>;
     otherwise: Expression;
 
-    constructor(key: string, inputType: Type, outputType: Type, input: Expression, branches: Branches, otherwise: Expression) {
+    constructor(key: string, inputType: Type, outputType: Type, input: Expression, cases: Cases, outputs: Array<Expression>, otherwise: Expression) {
         this.key = key;
         this.inputType = inputType;
         this.type = outputType;
         this.input = input;
-        this.branches = branches;
+        this.cases = cases;
+        this.outputs = outputs;
         this.otherwise = otherwise;
     }
 
     static parse(args: Array<mixed>, context: ParsingContext) {
-        args = args.slice(1);
-        if (args.length < 2)
-            return context.error(`Expected at least 2 arguments, but found only ${args.length}.`);
-        if (args.length % 2 !== 0)
+        if (args.length < 5)
+            return context.error(`Expected at least 4 arguments, but found only ${args.length - 1}.`);
+        if (args.length % 2 !== 1)
             return context.error(`Expected an even number of arguments.`);
 
         let inputType;
         let outputType = context.expectedType;
-        const branches = [];
-        for (let i = 1; i < args.length - 1; i += 2) {
+        const cases = {};
+        const outputs = [];
+        for (let i = 2; i < args.length - 1; i += 2) {
             let labels = args[i];
             const value = args[i + 1];
 
@@ -45,36 +48,41 @@ class Match implements Expression {
                 labels = [labels];
             }
 
-            const labelContext = context.concat(i + 1);
+            const labelContext = context.concat(i);
             if (labels.length === 0) {
                 return labelContext.error('Expected at least one branch label.');
             }
 
             for (const label of labels) {
-                if (label !== null && typeof label !== 'number' && typeof label !== 'string' && typeof label !== 'boolean') {
-                    return labelContext.error(`Branch labels must be null, numbers, strings, or booleans.`);
+                if (typeof label !== 'number' && typeof label !== 'string' && typeof label !== 'boolean') {
+                    return labelContext.error(`Branch labels must be numbers, strings, or booleans.`);
                 } else if (!inputType) {
                     inputType = typeOf(label);
-                } else if (match(inputType, typeOf(label), labelContext)) {
+                } else if (checkSubtype(inputType, typeOf(label), labelContext)) {
                     return null;
                 }
+
+                if (typeof cases[String(label)] !== 'undefined') {
+                    return labelContext.error('Branch labels must be unique.');
+                }
+
+                cases[String(label)] = outputs.length;
             }
 
-            const result = parseExpression(value, context.concat(i + 1, outputType));
+            const result = parseExpression(value, context.concat(i, outputType));
             if (!result) return null;
             outputType = outputType || result.type;
-
-            branches.push([(labels: any), result]);
+            outputs.push(result);
         }
 
-        const input = parseExpression(args[0], context.concat(1, inputType));
+        const input = parseExpression(args[1], context.concat(1, inputType));
         if (!input) return null;
 
-        const otherwise = parseExpression(args[args.length - 1], context.concat(args.length, outputType));
+        const otherwise = parseExpression(args[args.length - 1], context.concat(args.length - 1, outputType));
         if (!otherwise) return null;
 
         assert(inputType && outputType);
-        return new Match(context.key, (inputType: any), (outputType: any), input, branches, otherwise);
+        return new Match(context.key, (inputType: any), (outputType: any), input, cases, outputs, otherwise);
     }
 
     compile() {
@@ -82,27 +90,37 @@ class Match implements Expression {
         const outputs = [`function () { return ${this.otherwise.compile()} }.bind(this)`];
         const lookup = {};
 
-        for (const [labels, expression] of this.branches) {
-            for (const label of labels) {
-                lookup[`${typeOf(label).name}-${String(label)}`] = outputs.length;
-            }
-            outputs.push(`function () { return ${expression.compile()} }.bind(this)`);
+        for (const label in this.cases) {
+            // shift the index stored in this.cases by one, as we're using
+            // outputs[0] for the 'otherwise' case.
+            lookup[`${String(label)}`] = this.cases[label] + 1;
+        }
+        for (const output of this.outputs) {
+            outputs.push(`function () { return ${output.compile()} }.bind(this)`);
         }
 
         return `(function () {
             var o = [${outputs.join(', ')}];
             var l = ${JSON.stringify(lookup)};
             var i = ${input};
-            return o[l[this.typeOf(i) + '-' + i] || 0]();
+            return o[l[this.as(i, "${this.inputType.name}")] || 0]();
         }.bind(this))()`;
     }
 
     serialize() {
         const result = ['match'];
         result.push(this.input.serialize());
-        for (const [labels, expression] of this.branches) {
+        const branches = [];
+        for (const output of this.outputs) {
+            branches.push([[], output.serialize()]);
+        }
+        for (const label in this.cases) {
+            const index = this.cases[label];
+            branches[index][0].push(label);
+        }
+        for (const [labels, expression] of branches) {
             result.push(labels);
-            result.push(expression.serialize());
+            result.push(expression);
         }
         result.push(this.otherwise.serialize());
         return result;
@@ -111,8 +129,8 @@ class Match implements Expression {
     accept(visitor: Visitor<Expression>) {
         visitor.visit(this);
         this.input.accept(visitor);
-        for (const [ , expression] of this.branches) {
-            expression.accept(visitor);
+        for (const output of this.outputs) {
+            output.accept(visitor);
         }
         this.otherwise.accept(visitor);
     }
